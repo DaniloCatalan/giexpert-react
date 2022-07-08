9f9d369ba6d2194226e7e6635def92e2ded66202
Danilo <danilo.e.catalan@gmail.com>
2022-07-08 18:32:10 -0400

    add project

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+
+    if (categories.includes(newCategory)) return;
+
+    setCategories([...categories, newCategory]);
+  };
+
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+      {/* Listado de Gift */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))
+      }
+      {/* Gift Item */}
+    </>
+  );
+};
